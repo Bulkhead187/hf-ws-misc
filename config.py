9e2660de8b68b134432e4cb1dc8d8c62d3aaaec7
Bulkhead <danis2.wiebe@gmail.com>
2025-03-03 10:18:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import Field
+import os
+from dotenv import load_dotenv
+# hi ivan cyberbyte here
+load_dotenv()
 
 class Settings(BaseSettings):
-    NVIDIA_API_KEY: str
+    NVIDIA_API_KEY: str = Field(default=..., env="NVIDIA_API_KEY")
 
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 settings = Settings()
